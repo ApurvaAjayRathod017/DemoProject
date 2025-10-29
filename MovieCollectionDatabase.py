f61d81b29b3fdd40d23c0e3c3453ca9105201f6d
ApurvaR <apurvar1999@gmail.com>
2025-10-30 01:18:37 +0530

    added movies db

--- MovieCollectionDatabase.py
+++ MovieCollectionDatabase.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///C:/Python/movies.db"
+
+import os
+
+os.makedirs("data", exist_ok=True)
+DATABASE_URL = "sqlite:///./movies.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
